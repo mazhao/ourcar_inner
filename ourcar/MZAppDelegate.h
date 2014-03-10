057ecfb3af0e2280fd002d539ccd6141ca52d624
mazhao.mz <mazhao.mz@taobao.com>
2014-03-10 17:45:24 +0800

    Initial Commit

---
+++ ourcar/MZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MZAppDelegate.h
+//  ourcar
+//
+//  Created by mazhao on 14-3-10.
+//  Copyright (c) 2014年 MZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ourcar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ourcar
+//
+//  Created by mazhao on 14-3-10.
+//  Copyright (c) 2014年 MZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MZAppDelegate class]));
+    }
+}
